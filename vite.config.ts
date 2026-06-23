96660a89b579ec86a018a0bf0831093e8ea66b41
byadung <designer.adung@gmail.com>
2026-06-23 13:21:53 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/test/',
-  build: {
-    // CSS와 자바스크립트 빌드 파일들이 깨지지 않도록 자산 경로 구성을 최적화합니다.
-    cssCodeSplit: false,
-  }
 })
