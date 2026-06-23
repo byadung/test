61baaa7f6b70daccab19f1b817e93ff92e336f06
byadung <designer.adung@gmail.com>
2026-06-23 13:18:23 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
+import '../default_shadcn_theme.css' // 파일 위치에 맞게 경로 확인 (또는 './default_shadcn_theme.css')
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
