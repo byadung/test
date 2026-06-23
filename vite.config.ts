e11e37dca890c6f720a12b5ccc100ffdbe133e6c
byadung <designer.adung@gmail.com>
2026-06-23 13:17:06 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/test/', // 이 부분을 추가해야 깃허브 페이지가 올바른 경로로 파일을 읽어옵니다!
+  base: '/test/',
+  build: {
+    // CSS와 자바스크립트 빌드 파일들이 깨지지 않도록 자산 경로 구성을 최적화합니다.
+    cssCodeSplit: false,
+  }
 })
